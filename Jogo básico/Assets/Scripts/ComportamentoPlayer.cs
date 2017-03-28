e3b6d8e58bc53bacb3f7aa336663285cd8aea047
Paiba <pedropaivaladeira@hotmail.com>
2017-03-28 19:21:21 -0300

    Inimigo
    Criação de inimigo simples que faz uma patrulha

--- Jogo básico/Assets/Scripts/ComportamentoPlayer.cs
+++ Jogo básico/Assets/Scripts/ComportamentoPlayer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class pulo : MonoBehaviour {
+public class ComportamentoPlayer : MonoBehaviour {
 
     //Movement Variables 
     public float speedForce=20f;
---
+++ Jogo básico/Assets/Scripts/Patrulha.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrulha : MonoBehaviour {
+
+    public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+        if (transform.position.x >= 4.8 && transform.position.x <= 5)
+            moveSpeed *= -1;
+        if (transform.position.x >= -0.5 && transform.position.x <= -0.3)
+            moveSpeed *= -1;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
